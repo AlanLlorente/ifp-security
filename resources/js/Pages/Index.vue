51257e6a13357aa7614cb942f9a7291ad31b4b2a
kereketus <kereketus@gmail.com>
2022-06-03 17:37:27 +0200

    Update Index.vue
    ヽ(・∀・)ﾉ

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,58 +1,103 @@
 <template>
-  <layout>
-      <h1 class="mt-4 ml-4 text-3xl"><span class="text-lime-600">Hola </span><span class="text-sky-600">Alan!</span></h1>
-      <div class="flex grid grid-cols-2 gap-4 justify-items-center place-content-center h-full w-full space-y-32">
-          <div class="w-80">
-              <img src="https://www.advertisementshout.com/wp-content/uploads/2021/07/rick_astley_youtube_1627540038486.jpg" alt="imagen increible">
-          </div>
-          <div class="w-80">
-              <div class="bg-sky-600"><h3 class="text-white px-4 py-2">Novedades</h3></div>
-              <div class="flex justify-between mt-2">
-                  <div><img class="rounded-full h-6 w-6" src="https://learn.content.blackboardcdn.com/3900.39.0-rel.24+5903d92/images/ci/ng/default_profile_avatar.svg"></div>
-                  <div>Editar configuracion</div>
-                  <div>Acciones</div>
-              </div>
-              <div class="mt-2">
-                  <div>Anuncios</div>
-                  <div>Otro contenido nuevo</div>
-                  <div>Tablero de discursion</div>
-              </div>
-          </div>
-          <div>
-              <iframe width="160" height="315" src="https://www.youtube.com/embed/PQgVanDy-Ao" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-          <div class="w-80">
-              <div class="bg-sky-600"><h3 class="text-white px-4 py-2">Eventos de la semana</h3></div>
-              <div class="mt-8">
-                <table class="table-auto">
-                   <tbody>
-                    <tr>
-                        <td>12 de Junio</td>
-                        <td>CIBER_C_ENTREGA_UF2</td>
-                    </tr>
-                    <tr>
-                        <td>16 Junio</td>
-                        <td>LIMITE ENTREGA LENGUAJE DE MARCAS</td>
-                    </tr>
-                    <tr>
-                        <td>17 Junio</td>
-                        <td>CONFERENCIA CIBERSEGURIDAD</td>
-                   </tr>
-                    <tr>
-                        <td>30 Junio</td>
-                        <td>DESPLIEGUE SERVIDOR</td>
-                    </tr>
-                   </tbody>
-                </table>
-              </div>
-          </div>
-      </div>
-  </layout>
+    <layout>
+        <div class="mt-6 ml-4 px-4 text-2xl">
+            <span class="mr-1 text-lime-600">Hola</span
+            ><span class="text-sky-600">Alan!</span>
+        </div>
+        <div class="p-8">
+            <div class="flex justify-between">
+                <div class="mt-6">
+                    <img
+                        width="600"
+                        height="320"
+                        src="https://www.advertisementshout.com/wp-content/uploads/2021/07/rick_astley_youtube_1627540038486.jpg"
+                        alt="imagen increible"
+                    />
+                    <img
+                        width="600"
+                        height="320"
+                        src="https://i.ibb.co/SfyXBnp/enlaces.png"
+                        alt="imagen no tan increible"
+                    />
+                </div>
+
+                <div class="py-12 mr-60 ml-6">
+                    <div class="bg-sky-600 w-96 ml-4">
+                        <span
+                            ><h3 class="text-white py-1 px-4">
+                                Novedades
+                            </h3></span
+                        >
+                    </div>
+                    <div class="ml-6">
+                        <div class="py-2">
+                            <img
+                                width="30"
+                                height="30"
+                                src="https://learn.content.blackboardcdn.com/3900.39.0-rel.24+5903d92/images/ci/ng/default_profile_avatar.svg"
+                            />
+                        </div>
+                        <div class="py-2">
+                            <span class="font-bold mr-1">Anuncios</span
+                            ><span class="text-gray-500">(X)</span>
+                        </div>
+                        <div class="py-2">
+                            <span class="font-bold mr-1"
+                                >Libro de calificaciones</span
+                            ><span class="text-gray-500">(X)</span>
+                        </div>
+                        <div class="py-2">
+                            <span class="font-bold mr-1"
+                                >Otro contenido nuevo</span
+                            ><span class="text-gray-500">(X)</span>
+                        </div>
+                        <div class="py-2">
+                            <span class="font-bold mr-1"
+                                >Tablero de discusión</span
+                            ><span class="text-gray-500">(X)</span>
+                        </div>
+                    </div>
+                    <div class="text-gray-500 ml-4 py-6 px-26 text-xs pl-20">
+                        Última actualización: X de junio de 2022 XX:XX
+                    </div>
+                    <div class="bg-sky-600 object-cover w-96 ml-4">
+                        <span
+                            ><h3 class="text-white py-1 px-4">
+                                Eventos de la semana
+                            </h3></span
+                        >
+                    </div>
+                    <div class="mr-8">
+                        <img
+                            width="550"
+                            height="320"
+                            src="https://i.ibb.co/JqNL0gG/inferior.png"
+                            alt="imagen increible"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-between">
+                <div class="ml-10 mb-8">
+                    <iframe
+                        width="450"
+                        height="280"
+                        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen
+                    ></iframe>
+                </div>
+                <div></div>
+            </div>
+        </div>
+    </layout>
 </template>
 <script>
 import Layout from "./Resources/Layout";
 
-export default{
-    components: {Layout}
-}
+export default {
+    components: { Layout },
+};
 </script>
