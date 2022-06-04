6e92822875993abb59a4405c3bdf3d23e5871f6b
kereketus <kereketus@gmail.com>
2022-06-04 14:12:03 +0200

    Update Index.vue
    ╰(▔∀▔)╯

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,28 +1,89 @@
 <template>
     <layout>
-        <div class="mt-6 ml-4 px-4 text-2xl">
-            <span class="mr-1 text-lime-600">Hola</span
+        <div class="text-2xl mt-6 ml-4 px-4">
+            <span class="text-lime-600 mr-1">Hola</span
             ><span class="text-sky-600">Alan!</span>
         </div>
-        <div class="p-8">
+        <div class="p-10 -mr-30">
             <div class="flex justify-between">
-                <div class="mt-6">
+                <div class="mt-6 -mr-20">
                     <img
                         width="600"
                         height="320"
                         src="https://www.advertisementshout.com/wp-content/uploads/2021/07/rick_astley_youtube_1627540038486.jpg"
                         alt="imagen increible"
                     />
-                    <img
-                        width="600"
-                        height="320"
-                        src="https://i.ibb.co/SfyXBnp/enlaces.png"
-                        alt="imagen no tan increible"
-                    />
+                    <div class="bg-sky-600">
+                        <div class="text-white px-2 py-1">Enlaces</div>
+                        <div class="text-white -mt-5 px-2 py-2">Rápidos</div>
+                    </div>
+
+                    <div class="flex justify-center gap-6">
+                        <div class="-mt-10 w-6 h-6">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                            >
+                                <path
+                                    d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 128c39.77 0 72 32.24 72 72S295.8 272 256 272c-39.76 0-72-32.24-72-72S216.2 128 256 128zM256 448c-52.93 0-100.9-21.53-135.7-56.29C136.5 349.9 176.5 320 224 320h64c47.54 0 87.54 29.88 103.7 71.71C356.9 426.5 308.9 448 256 448z"
+                                />
+                            </svg>
+                        </div>
+                        <div class="-mt-10 w-6 h-6">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                            >
+                                <path
+                                    d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 400c-18 0-32-14-32-32s13.1-32 32-32c17.1 0 32 14 32 32S273.1 400 256 400zM325.1 258L280 286V288c0 13-11 24-24 24S232 301 232 288V272c0-8 4-16 12-21l57-34C308 213 312 206 312 198C312 186 301.1 176 289.1 176h-51.1C225.1 176 216 186 216 198c0 13-11 24-24 24s-24-11-24-24C168 159 199 128 237.1 128h51.1C329 128 360 159 360 198C360 222 347 245 325.1 258z"
+                                />
+                            </svg>
+                        </div>
+                        <div class="-mt-10 w-6 h-6">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                            >
+                                <path
+                                    d="M0 96C0 60.65 28.65 32 64 32H448C483.3 32 512 60.65 512 96V416C512 451.3 483.3 480 448 480H64C28.65 480 0 451.3 0 416V96zM64 160H128V96H64V160zM448 96H192V160H448V96zM64 288H128V224H64V288zM448 224H192V288H448V224zM64 416H128V352H64V416zM448 352H192V416H448V352z"
+                                />
+                            </svg>
+                        </div>
+                        <div class="-mt-10 w-6 h-6">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 448 512"
+                            >
+                                <path
+                                    d="M256 32V51.2C329 66.03 384 130.6 384 208V226.8C384 273.9 401.3 319.2 432.5 354.4L439.9 362.7C448.3 372.2 450.4 385.6 445.2 397.1C440 408.6 428.6 416 416 416H32C19.4 416 7.971 408.6 2.809 397.1C-2.353 385.6-.2883 372.2 8.084 362.7L15.5 354.4C46.74 319.2 64 273.9 64 226.8V208C64 130.6 118.1 66.03 192 51.2V32C192 14.33 206.3 0 224 0C241.7 0 256 14.33 256 32H256zM224 512C207 512 190.7 505.3 178.7 493.3C166.7 481.3 160 464.1 160 448H288C288 464.1 281.3 481.3 269.3 493.3C257.3 505.3 240.1 512 224 512z"
+                                />
+                            </svg>
+                        </div>
+                        <div class="-mt-10 w-6 h-6">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                            >
+                                <path
+                                    d="M495.9 166.6C499.2 175.2 496.4 184.9 489.6 191.2L446.3 230.6C447.4 238.9 448 247.4 448 256C448 264.6 447.4 273.1 446.3 281.4L489.6 320.8C496.4 327.1 499.2 336.8 495.9 345.4C491.5 357.3 486.2 368.8 480.2 379.7L475.5 387.8C468.9 398.8 461.5 409.2 453.4 419.1C447.4 426.2 437.7 428.7 428.9 425.9L373.2 408.1C359.8 418.4 344.1 427 329.2 433.6L316.7 490.7C314.7 499.7 307.7 506.1 298.5 508.5C284.7 510.8 270.5 512 255.1 512C241.5 512 227.3 510.8 213.5 508.5C204.3 506.1 197.3 499.7 195.3 490.7L182.8 433.6C167 427 152.2 418.4 138.8 408.1L83.14 425.9C74.3 428.7 64.55 426.2 58.63 419.1C50.52 409.2 43.12 398.8 36.52 387.8L31.84 379.7C25.77 368.8 20.49 357.3 16.06 345.4C12.82 336.8 15.55 327.1 22.41 320.8L65.67 281.4C64.57 273.1 64 264.6 64 256C64 247.4 64.57 238.9 65.67 230.6L22.41 191.2C15.55 184.9 12.82 175.3 16.06 166.6C20.49 154.7 25.78 143.2 31.84 132.3L36.51 124.2C43.12 113.2 50.52 102.8 58.63 92.95C64.55 85.8 74.3 83.32 83.14 86.14L138.8 103.9C152.2 93.56 167 84.96 182.8 78.43L195.3 21.33C197.3 12.25 204.3 5.04 213.5 3.51C227.3 1.201 241.5 0 256 0C270.5 0 284.7 1.201 298.5 3.51C307.7 5.04 314.7 12.25 316.7 21.33L329.2 78.43C344.1 84.96 359.8 93.56 373.2 103.9L428.9 86.14C437.7 83.32 447.4 85.8 453.4 92.95C461.5 102.8 468.9 113.2 475.5 124.2L480.2 132.3C486.2 143.2 491.5 154.7 495.9 166.6V166.6zM256 336C300.2 336 336 300.2 336 255.1C336 211.8 300.2 175.1 256 175.1C211.8 175.1 176 211.8 176 255.1C176 300.2 211.8 336 256 336z"
+                                />
+                            </svg>
+                        </div>
+                        <div class="-mt-10 w-6 h-6">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                            >
+                                <path
+                                    d="M199.1 272C199.1 263.2 207.2 256 215.1 256C224.8 256 231.1 263.2 231.1 272C231.1 280.8 224.8 288 215.1 288C207.2 288 199.1 280.8 199.1 272zM312 272C312 280.8 304.8 288 296 288C287.2 288 280 280.8 280 272C280 263.2 287.2 256 296 256C304.8 256 312 263.2 312 272zM256.3-.0068C261.9-.0507 267.3 1.386 272.1 4.066L476.5 90.53C487.7 95.27 495.2 105.1 495.9 118.1C501.6 213.6 466.7 421.9 272.5 507.7C267.6 510.5 261.1 512.1 256.3 512C250.5 512.1 244.9 510.5 239.1 507.7C45.8 421.9 10.95 213.6 16.57 118.1C17.28 105.1 24.83 95.27 36.04 90.53L240.4 4.066C245.2 1.386 250.7-.0507 256.3-.0068H256.3zM223.1 208L159.1 144V272C159.1 325 202.1 368 255.1 368C309 368 352 325 352 272V144L288 208H223.1z"
+                                />
+                            </svg>
+                        </div>
+                    </div>
                 </div>
 
-                <div class="py-12 mr-60 ml-6">
-                    <div class="bg-sky-600 w-96 ml-4">
+                <div class="py-12 px-20">
+                    <div class="bg-sky-600 ml-4">
                         <span
                             ><h3 class="text-white py-1 px-4">
                                 Novedades
@@ -32,53 +93,118 @@
                     <div class="ml-6">
                         <div class="py-2">
                             <img
-                                width="30"
-                                height="30"
+                                class="w-7 h-7"
                                 src="https://learn.content.blackboardcdn.com/3900.39.0-rel.24+5903d92/images/ci/ng/default_profile_avatar.svg"
                             />
+                            <div class="flex justify-end -mt-8">
+                                <img
+                                    width="300"
+                                    height="400"
+                                    src="https://i.ibb.co/y442HX5/botones.png"
+                                />
+                            </div>
                         </div>
-                        <div class="py-2">
+                        <div class="py-2 px-3">
+                            <div class="w-2 h-2 fill-gray-500 -mb-3.5 -ml-3">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 256 512"
+                                >
+                                    <path
+                                        d="M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z"
+                                    />
+                                </svg>
+                            </div>
                             <span class="font-bold mr-1">Anuncios</span
                             ><span class="text-gray-500">(X)</span>
                         </div>
-                        <div class="py-2">
+                        <div class="py-2 px-3">
+                            <div class="w-2 h-2 fill-gray-500 -mb-3.5 -ml-3">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 256 512"
+                                >
+                                    <path
+                                        d="M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z"
+                                    />
+                                </svg>
+                            </div>
+                            <span class="font-bold mr-1">Evaluaciones</span
+                            ><span class="text-gray-500">(X)</span>
+                        </div>
+                        <div class="py-2 px-3">
+                            <div class="w-2 h-2 fill-gray-500 -mb-3.5 -ml-3">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 256 512"
+                                >
+                                    <path
+                                        d="M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z"
+                                    />
+                                </svg>
+                            </div>
+
                             <span class="font-bold mr-1"
                                 >Libro de calificaciones</span
                             ><span class="text-gray-500">(X)</span>
                         </div>
-                        <div class="py-2">
+                        <div class="py-2 px-3">
+                            <div class="w-2 h-2 fill-gray-500 -mb-3.5 -ml-3">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 256 512"
+                                >
+                                    <path
+                                        d="M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z"
+                                    />
+                                </svg>
+                            </div>
                             <span class="font-bold mr-1"
                                 >Otro contenido nuevo</span
                             ><span class="text-gray-500">(X)</span>
                         </div>
-                        <div class="py-2">
+                        <div class="py-2 px-3">
+                            <div class="w-2 h-2 fill-gray-500 -mb-3.5 -ml-3">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 256 512"
+                                >
+                                    <path
+                                        d="M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z"
+                                    />
+                                </svg>
+                            </div>
                             <span class="font-bold mr-1"
                                 >Tablero de discusión</span
                             ><span class="text-gray-500">(X)</span>
                         </div>
                     </div>
-                    <div class="text-gray-500 ml-4 py-6 px-26 text-xs pl-20">
-                        Última actualización: X de junio de 2022 XX:XX
+                    <div class="text-gray-500 text-xs flex justify-end mb-8">
+                        <div>
+                            Última actualización: X de junio de 2022 XX:XX
+                        </div>
                     </div>
-                    <div class="bg-sky-600 object-cover w-96 ml-4">
+                    <div class="bg-sky-600 object-cover ml-4">
                         <span
                             ><h3 class="text-white py-1 px-4">
                                 Eventos de la semana
                             </h3></span
                         >
                     </div>
-                    <div class="mr-8">
-                        <img
-                            width="550"
-                            height="320"
-                            src="https://i.ibb.co/JqNL0gG/inferior.png"
-                            alt="imagen increible"
-                        />
+                    <div class="flex justify-end -mr-12">
+                        <div>
+                            <img
+                                width="550"
+                                height="320"
+                                src="https://i.ibb.co/JqNL0gG/inferior.png"
+                                alt="imagen nada increible"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="flex justify-between">
-                <div class="ml-10 mb-8">
+                <div class="ml-10 -mt-20">
                     <iframe
                         width="450"
                         height="280"
